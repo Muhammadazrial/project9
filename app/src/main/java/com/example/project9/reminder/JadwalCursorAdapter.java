2847319eeafb5fd90c6a0718d58b3f3afc66d7d5
Muhammad234 <60900119028@uin-alauddin.ac.id>
2021-12-12 21:25:13 +0800

    Commit-Progress-Pertama

---
+++ app/src/main/java/com/example/project9/reminder/JadwalCursorAdapter.java
@@ -0,0 +1,4 @@
+package com.example.project9.reminder;
+
+public class JadwalCursorAdapter {
+}
---
+++ app/src/main/java/com/example/project9/reminder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.project9.reminder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.project9.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
